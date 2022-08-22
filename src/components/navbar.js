aa834fbce1de192d60323938c4a2fec79d21f15a
VELASQUEZ Ale <alevzdeveloper@gmail.com>
2022-08-22 15:20:13 -0400

    agregando navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,25 @@
+
+
+export const Navbar = () => {
+
+    return (
+    <header className="App-header">
+
+        <nav>
+          <ul>
+            <li>Mangas</li>
+            <li>Libros</li>
+            <li>Quienes somos</li>
+          </ul>
+        </nav>
+    </header>
+    )
+}
+
+export const Logo = () => {
+  return (
+    <img src="readLogo.png"/>
+  )
+} 
+
+// export default Navbar
---
+++ src/components/phrase.js
@@ -0,0 +1,13 @@
+
+export const Phrase = () => {
+  return (
+  <main className="mainbody">
+    <p className="glitch">
+      Siempre imaginé que el paraíso sería algún tipo de biblioteca 
+       Jorge Luis Borges. 
+    </p>
+  </main>
+  )
+};
+
+
